036ace3762e872f646d826eba6df8fed9acbab6b
XMS_JZhan <xms_jzhan@XMS-JZhan.local>
2019-02-12 19:12:45 -0700

    Initial Commit

---
+++ RedditObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditObjC
+//
+//  Created by XMS_JZhan on 2/12/19.
+//  Copyright © 2019 XMS_JZhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedditObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedditObjC
+//
+//  Created by XMS_JZhan on 2/12/19.
+//  Copyright © 2019 XMS_JZhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
